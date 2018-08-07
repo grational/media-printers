84376856e93931e656d4bc704b2caf256eff92a6
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2018-08-07 12:29:23 +0200

    Extend the Media interface to cover the all the with cases
    The previously named merge method has been transformed into with(Media)

--- src/main/groovy/it/italiaonline/rnd/printers/Media.groovy
+++ src/main/groovy/it/italiaonline/rnd/printers/Media.groovy
@@ -12,7 +12,69 @@ interface Media {
    * @param name  a String label (key)
    * @param value an object with the content of the field (value)
    * @return Media  return an object Media that contains all the couples
+   * @throws UnsupportedOperationException
    */
   Media with(String name, def value)
+
+  /**
+   * This method load a Map into the class accumulator
+   * <p>
+   * @param data the Map to be loaded into the class accumulator
+   * @return Media  return an object Media that contains also the map
+   * @throws UnsupportedOperationException
+   */
+	Media with(Map data)
+
+  /**
+   * This method load a List into the class accumulator
+   * <p>
+   * @param data the List to be loaded into the class accumulator
+   * @return Media  return an object Media that contains also the list
+   * @throws UnsupportedOperationException
+   */
+	Media with(List data)
+
+  /**
+   * This method load a Number into the class accumulator
+   * <p>
+   * @param data the Map to be loaded into the class accumulator
+   * @return Media  return an object Media that contains also the number
+   * @throws UnsupportedOperationException
+   */
+	Media with(Number data)
+
+  /**
+   * This method load a String into the class accumulator
+   * <p>
+   * @param data the Map to be loaded into the class accumulator
+   * @return Media  return an object Media that contains also the string
+   * @throws UnsupportedOperationException
+   */
+	Media with(String data)
+
+  /**
+   * This method load a Boolean into the class accumulator
+   * <p>
+   * @param data the Map to be loaded into the class accumulator
+   * @return Media  return an object Media that contains also the boolean
+   * @throws UnsupportedOperationException
+   */
+	Media with(Boolean data)
+
+  /**
+   * This method load another Media into the class accumulator
+   * <p>
+   * @param another a compatible media
+   * @return Media  return an object Media that contains also the second Media
+   * @throws UnsupportedOperationException
+   */
+  Media with(Media another)
+
+  /**
+   * This method return a class rapresentation of the accumulator
+   * <p>
+   * @return Object return an object rapresentation of the Media content
+   */
+  def structure()
 
 }
